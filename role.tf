8a393d8479db9f5e19d381c42e9164e1806e891a
Jon <jon@rivingtondigital.com>
2020-02-25 10:50:01 -0800

    add route53 permissions to instance role

--- role.tf
+++ role.tf
@@ -21,4 +21,41 @@ resource "aws_iam_role" "default_role" {
 EOF
 }
 
+  
+resource "aws_iam_policy" "policy" {
+  name        = "${var.project_domain}-route53-policy"
+  description = "give route53 access to instance for letsencrypt dns"
+  
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListHostedZones",
+                "route53:GetChange"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect" : "Allow",
+            "Action" : [
+                "route53:ChangeResourceRecordSets"
+            ],
+            "Resource" : [
+                "arn:aws:route53:::hostedzone/Z125YGKA7OYJSF"
+            ]
+          }
+      ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "route53_attach" {
+  role        = aws_iam_role.default_role.name
+  policy_arn  = aws_iam_policy.policy.arn 
+}
 
